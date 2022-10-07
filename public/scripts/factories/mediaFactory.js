9462aa68b7d85b9285f67b9faf16771fd2fbbdaf
Marine <marine.magnin73@gmail.com>
2022-10-07 10:19:58 +0200

    Organisation : ajout d'un fichier _card-media.scss pour le css des médias

--- public/scripts/factories/mediaFactory.js
+++ public/scripts/factories/mediaFactory.js
@@ -3,6 +3,10 @@ function photographerMediasFactory(photographersMedias) {
 
     const params = new URL(document.location).searchParams; // Je récupère les paramètres de mon url
     const nom = params.get("name"); // Je récupère le nom du photographe
+    const idURL = parseInt(params.get("id"), 10); // Je récupère la valeur associée à mon id
+
+    // const photographerId = idURL;
+    // console.log(photographerId);
 
     const mediaPhoto = `./assets/photographers/${nom}/${image}`;
     const mediaVideo = `./assets/photographers/${nom}/${video}`;
@@ -22,6 +26,7 @@ function photographerMediasFactory(photographersMedias) {
         // img.className = "card__media--img";
         img.setAttribute("src", mediaPhoto);
         img.setAttribute("alt", title);
+        img.className = "card__media--img";
 
       // Ajout des éléments dans la div1 : banner
       article.appendChild(titre);
