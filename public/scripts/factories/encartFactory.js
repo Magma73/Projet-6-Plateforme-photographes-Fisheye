d9568e7e4341a5eca7d24a8b022d328d93031bcf
Marine <marine.magnin73@gmail.com>
2022-10-10 20:06:17 +0200

    Style : création de l'affichage de l'encart des likes

--- public/scripts/factories/encartFactory.js
+++ public/scripts/factories/encartFactory.js
@@ -5,39 +5,38 @@ function encartMediasFactory(photographers) {
 
     function getEncartCardDOM() {
 
-    // Création du conteneur du nb total des likes
+    // Création du conteneur encart
     const containerEncart = document.createElement("div");
     containerEncart.className = "container__encart";
 
-    // Création du conteneur du nb total des likes
+    // Création du conteneur des likes
     const encartLikes = document.createElement("div");
     encartLikes.className = "encart__likes";
 
-    // Création du conteneur du tarfi journalier
+    // Création du conteneur du tarif journalier
     const encartTarif = document.createElement("div");
     encartTarif.className = "encart__tarif";
 
     // Création du nombre total de likes
     const nbTotalLikes = document.createElement("p");
     nbTotalLikes.className = "encart__likes--total";
-    nbTotalLikes.textContent = "123";
+    nbTotalLikes.textContent = "297 081";
 
     // Création de l'icône
     const iconTotalLikes = document.createElement("i");
     iconTotalLikes.className = "fas fa-heart encart__likes--heart";
 
-    // Création du conteneur du tarfi journalier
+    // Création du tarif journalier
     const encartTarifJour = document.createElement("p");
     encartTarifJour.className = "encart__tarifJour";
-    encartTarifJour.textContent = price + "€/jour";
+    encartTarifJour.textContent = price + "€ / jour";
 
-    // Ajout du nombre de likes et de l'icône dans le conteneur nb total des likes
+    // Ajout du nombre de likes et de l'icône dans le conteneur des likes
     encartLikes.appendChild(nbTotalLikes);
     encartLikes.appendChild(iconTotalLikes);
 
     // Ajout du tarif journalier dans le conteneur encartTarif
     encartTarif.appendChild(encartTarifJour);
-
 
     // Ajout des éléments encartLikes et encartTarif dans containerEncart
     containerEncart.appendChild(encartLikes);
