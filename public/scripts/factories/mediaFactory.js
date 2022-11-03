74440636e989b607e75c0544ae0838514308a684
Marine <marine.magnin73@gmail.com>
2022-11-03 17:05:07 +0100

    Fonctionnalités :  ajout de l'id de l'image dans la card__media

--- public/scripts/factories/mediaFactory.js
+++ public/scripts/factories/mediaFactory.js
@@ -18,6 +18,7 @@ function photographerMediasFactory(photographersMedias) {
       // Création du conteneur article
       const article = document.createElement("article");
       article.className = "card__media";
+      article.setAttribute("id", id);
 
       // Création du conteneur media element
       const mediaElement = document.createElement("div");
@@ -66,6 +67,8 @@ function photographerMediasFactory(photographersMedias) {
       // Création de l'icône
       const icon = document.createElement("i");
       icon.className = "fas fa-heart card__media--icon-heart";
+      icon.setAttribute("aria-label", "Nombre de likes par média");
+      icon.setAttribute("tabindex", "0");
       // icon.textContent =
       // "<path d='M9.5 18.35L8.23125 17.03C3.725 12.36 0.75 9.28 0.75 5.5C0.75 2.42 2.8675 0 5.5625 0C7.085 0 8.54625 0.81 9.5 2.09C10.4537 0.81 11.915 0 13.4375 0C16.1325 0 18.25 2.42 18.25 5.5C18.25 9.28 15.275 12.36 10.7688 17.04L9.5 18.35Z' fill='#911C1C'/>";
 
