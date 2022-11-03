7cc37688d3ef0c881ea2cf3fe620d5c8a8cb5df8
Marine <marine.magnin73@gmail.com>
2022-11-03 17:05:47 +0100

    Fonctionnalités : simplification de la lightbox

--- public/scripts/factories/lightboxFactory.js
+++ public/scripts/factories/lightboxFactory.js
@@ -22,74 +22,71 @@ function lightboxMediasFactory(photographersMedias) {
       // Création de la div contenant la croix
       const carrouselClose = document.createElement("div");
       carrouselClose.className = "carrousel__close";
+      carrouselClose.setAttribute("aria-label", "Fermer le carrousel");
+      // carrouselClose.setAttribute("role", "button");
+      carrouselClose.setAttribute("tabindex", 7);
 
       // Création de l'icône croix
       const carrouselCross = document.createElement("img");
       carrouselCross.className = "carrousel__cross";
       carrouselCross.setAttribute("src", "./assets/icons/close-red.svg");
       carrouselCross.setAttribute("alt", "Fermer le carrousel");
-      carrouselCross.setAttribute("tabindex", 6);
+      // carrouselCross.setAttribute("tabindex", 7);
 
       // CONTRÔLES "PRECENDANTE"
       // Création de la div contenant le bouton de contrôle "Précédante"
       const carrouselControlsLeft = document.createElement("div");
       carrouselControlsLeft.className = "carrousel__controls carrousel__controls--left";
-      carrouselControlsLeft.setAttribute("role", "button");
+      // carrouselControlsLeft.setAttribute("role", "button");
+      carrouselControlsLeft.setAttribute("aria-label", "Image précédante");
+      carrouselControlsLeft.setAttribute("tabindex", 5);
 
       // Création de la span contenant l'icône et le mot "Précédante"
       const carrouselArrowLeft = document.createElement("span");
       carrouselArrowLeft.className = "carrousel__arrow carrousel__arrow--prev";
-      carrouselArrowLeft.setAttribute("tabindex", 4);
+      carrouselArrowLeft.setAttribute("aria-label", "Image précédante");
 
       // Création de l'icône flèche précédante
       const carrouselIconLeft = document.createElement("i");
       carrouselIconLeft.className = "fas fa-chevron-left";
       carrouselIconLeft.setAttribute("aria-hidden", "true");
 
-      // Création du mot "Précédante"
-      const carrouselTextPrev = document.createElement("p");
-      carrouselTextPrev.className = "sr-only";
-      carrouselTextPrev.textContent = "Précédante";
-
       // CONTRÔLES "SUIVANTE"
       // Création de la div contenant le bouton de contrôle "Suivante"
       const carrouselControlsRight = document.createElement("div");
       carrouselControlsRight.className = "carrousel__controls carrousel__controls--right";
-      carrouselControlsRight.setAttribute("role", "button");
+      // carrouselControlsRight.setAttribute("role", "button");
+      carrouselControlsRight.setAttribute("aria-label", "Image suivante");
+      carrouselControlsRight.setAttribute("tabindex", 6);
 
       // Création de la span contenant l'icône et le mot "Suivante"
       const carrouselArrowRight = document.createElement("span");
       carrouselArrowRight.className = "carrousel__arrow carrousel__arrow--next";
-      carrouselArrowRight.setAttribute("tabindex", 5);
+      carrouselArrowRight.setAttribute("aria-label", "Image suivante");
 
       // Création de l'icône flèche précédante
       const carrouselIconRight = document.createElement("i");
       carrouselIconRight.className = "fas fa-chevron-right";
       carrouselIconRight.setAttribute("aria-hidden", "true");
 
-      // Création du mot "Précédante"
-      const carrouselTextNext = document.createElement("p");
-      carrouselTextNext.className = "sr-only";
-      carrouselTextNext.textContent = "Suivante";
-
       // MEDIAS
       // Création de la div contenant les médias : image ou vidéo
       const carrouselElement = document.createElement("div");
       carrouselElement.className = "carrousel__element";
+      carrouselElement.setAttribute("tabindex", 2);
 
       // Création du titre des médias
       const carrouselTitle = document.createElement("h1");
       carrouselTitle.className = "carrousel__title";
-      carrouselTitle.setAttribute("alt", title);
       carrouselTitle.textContent = title;
-      carrouselElement.setAttribute("tabindex", 3);
+      carrouselTitle.setAttribute("tabindex", 4);
 
       // Création du média photo ou video et ajout dans la div mediaElement
       if (video !== undefined) {
          const video = document.createElement("video");
          video.setAttribute("src", mediaVideo);
          video.setAttribute("alt", title);
-         video.setAttribute("tabindex", 2);
+         video.setAttribute("tabindex", 3);
          video.controls = true;
          video.className = "carrousel__media carrousel__media--video";
          carrouselElement.appendChild(video);
@@ -98,7 +95,7 @@ function lightboxMediasFactory(photographersMedias) {
          const img = document.createElement("img");
          img.setAttribute("src", mediaPhoto);
          img.setAttribute("alt", title);
-         img.setAttribute("tabindex", 2);
+         img.setAttribute("tabindex", 3);
          img.className = "carrousel__media carrousel__media--img";
          carrouselElement.appendChild(img);
          carrouselElement.appendChild(carrouselTitle);
@@ -114,14 +111,12 @@ function lightboxMediasFactory(photographersMedias) {
 
       // Ajout des éléments dans la div carrouselControlsLeft
       carrouselControlsLeft.appendChild(carrouselArrowLeft);
-      carrouselControlsLeft.appendChild(carrouselTextPrev);
 
       // Ajout de l'icône carrouselIconRight dans la span carrouselArrowRight
       carrouselArrowRight.appendChild(carrouselIconRight);
 
       // Ajout des éléments dans la div carrouselControlsRight
       carrouselControlsRight.appendChild(carrouselArrowRight);
-      carrouselControlsRight.appendChild(carrouselTextNext);
 
       // Ajout des éléments dans la div carrouselItem : carrouselClose + carrouselControls + carrouselElement
       carrouselItem.appendChild(carrouselClose);
