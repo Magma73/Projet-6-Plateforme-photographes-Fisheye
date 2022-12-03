5cf9f5bdc038e7977d85f4a8de5619a563b0d46f
Marine <marine.magnin73@gmail.com>
2022-12-03 17:10:53 +0100

    Vérification du code : suppression du code inutile + mise en page avec prettier

--- public/scripts/pages/index.js
+++ public/scripts/pages/index.js
@@ -1,36 +1,35 @@
 async function getPhotographers() {
-  try {
-    const response = await fetch("../data/photographers.json");
-    // console.log(response);
-    if (response.status === 200) {
-      const dataPhotographers = await response.json();
-      const photographers = dataPhotographers.photographers;
-      // et bien retourner le tableau photographers seulement une fois
-      return ({
-        photographers: [...photographers],
-      });
-    } else {
-      throw "Le fichier JSON n'a pas été trouvé";
-    }
-  } catch (error) {
-    throw error;
-  }
+   try {
+      const response = await fetch("../data/photographers.json");
+      if (response.status === 200) {
+         const dataPhotographers = await response.json();
+         const photographers = dataPhotographers.photographers;
+         // et bien retourner le tableau photographers seulement une fois
+         return {
+            photographers: [...photographers],
+         };
+      } else {
+         throw "Le fichier JSON n'a pas été trouvé";
+      }
+   } catch (error) {
+      throw error;
+   }
 }
 
 async function displayData(photographers) {
-  const photographersSection = document.querySelector(".photographer__section");
+   const photographersSection = document.querySelector(".photographer__section");
 
-  photographers.forEach((photographer) => {
-    const photographerModel = photographerFactory(photographer);
-    const userCardDOM = photographerModel.getUserCardDOM();
-    photographersSection.appendChild(userCardDOM);
-  });
+   photographers.forEach((photographer) => {
+      const photographerModel = photographerFactory(photographer);
+      const userCardDOM = photographerModel.getUserCardDOM();
+      photographersSection.appendChild(userCardDOM);
+   });
 }
 
 async function init() {
-  // Récupère les datas des photographes
-  const { photographers } = await getPhotographers();
-  displayData(photographers);
+   // Récupère les datas des photographes
+   const { photographers } = await getPhotographers();
+   displayData(photographers);
 }
 
 init();
